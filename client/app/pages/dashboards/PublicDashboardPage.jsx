e4c989965eb1a9493464427853a2935becb436f2
kettlelinna <13501568940@163.com>
2026-01-26 23:41:02 +0800

    add import wx

--- client/app/pages/dashboards/PublicDashboardPage.jsx
+++ client/app/pages/dashboards/PublicDashboardPage.jsx
@@ -1,6 +1,7 @@
 import { isEmpty } from "lodash";
 import React from "react";
 import PropTypes from "prop-types";
+import wx from "weixin-js-sdk";
 
 import routeWithApiKeySession from "@/components/ApplicationArea/routeWithApiKeySession";
 import Link from "@/components/Link";
