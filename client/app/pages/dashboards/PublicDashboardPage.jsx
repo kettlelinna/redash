7fd0c4f74221f25316e0610d5cecff85ef217add
kettlelinna <13501568940@163.com>
2026-01-27 11:17:50 +0800

    Revert "revert wx"
    This reverts commit 6fd33c3badf4509ef6fa10135e1a7ac8e3322dab.

--- client/app/pages/dashboards/PublicDashboardPage.jsx
+++ client/app/pages/dashboards/PublicDashboardPage.jsx
@@ -1,8 +1,11 @@
 import { isEmpty } from "lodash";
 import React from "react";
 import PropTypes from "prop-types";
+import wx from "weixin-js-sdk";
+import Button from "antd/lib/button";
 
 import routeWithApiKeySession from "@/components/ApplicationArea/routeWithApiKeySession";
+import Link from "@/components/Link";
 import BigMessage from "@/components/BigMessage";
 import PageHeader from "@/components/PageHeader";
 import Parameters from "@/components/Parameters";
@@ -11,6 +14,8 @@ import Filters from "@/components/Filters";
 
 import { Dashboard } from "@/services/dashboard";
 import routes from "@/services/routes";
+
+import logoUrl from "@/assets/images/redash_icon_small.png";
 
 import useDashboard from "./hooks/useDashboard";
 
@@ -75,6 +80,11 @@ class PublicDashboardPage extends React.Component {
       .catch(error => this.props.onError(error));
   }
 
+  onClickBack = () => {
+    wx.miniProgram.navigateTo({url: '/pages/newindex/newindex'});
+    //wx.miniProgram.redirectTo({url: '/pages/newindex/newindex'});
+  }
+
   render() {
     const { loading, dashboard } = this.state;
     return (
@@ -86,6 +96,11 @@ class PublicDashboardPage extends React.Component {
         ) : (
           <PublicDashboard dashboard={dashboard} />
         )}
+        <div id="footer">
+          <div className="text-center">
+            <Button type="primary" className="w-100" onClick={this.onClickBack}>Back</Button>
+          </div>
+        </div>
       </div>
     );
   }
