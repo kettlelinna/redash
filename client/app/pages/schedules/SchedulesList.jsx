c09316352860f95292f28dfc070abfe8214ef175
kettlelinna <13501568940@163.com>
2026-02-03 11:00:46 +0800

    add schedule page - fix bug

--- client/app/pages/schedules/SchedulesList.jsx
+++ client/app/pages/schedules/SchedulesList.jsx
@@ -1,29 +1,26 @@
+import { isEmpty, reject } from "lodash";
 import React from "react";
+import PropTypes from "prop-types";
 
 import Button from "antd/lib/button";
 import routeWithUserSession from "@/components/ApplicationArea/routeWithUserSession";
-import Link from "@/components/Link";
 import navigateTo from "@/components/ApplicationArea/navigateTo";
-import Paginator from "@/components/Paginator";
-
-import { wrap as itemsList, ControllerType } from "@/components/items-list/ItemsList";
-import { ResourceItemsSource } from "@/components/items-list/classes/ItemsSource";
-import { StateStorage } from "@/components/items-list/classes/StateStorage";
-
+import CardsList from "@/components/cards-list/CardsList";
 import LoadingState from "@/components/items-list/components/LoadingState";
-import EmptyState from "@/components/items-list/components/EmptyState";
-import ItemsTable, { Columns } from "@/components/items-list/components/ItemsTable";
-
-import CreateScheduleDialog from "@/components/schedules/CreateScheduleDialog";
-import DeleteScheduleButton from "@/components/schedules/DeleteScheduleButton";
-
-import Schedule from "@/services/schedule";
-import { currentUser } from "@/services/auth";
-import routes from "@/services/routes";
+import CreateSourceDialog from "@/components/CreateSourceDialog";
+import DynamicComponent, { registerComponent } from "@/components/DynamicComponent";
+import helper from "@/components/dynamic-form/dynamicFormHelper";
 import wrapSettingsTab from "@/components/SettingsWrapper";
+import PlainButton from "@/components/PlainButton";
 
+import DataSource, { IMG_ROOT } from "@/services/data-source";
+import { policy } from "@/services/policy";
+import recordEvent from "@/services/recordEvent";
+import routes from "@/services/routes";
+import {ControllerType} from "@/components/items-list/ItemsList";
+import {Columns} from "@/components/items-list/components/ItemsTable";
 
-class SchedulesList extends React.Component {
+class SchedulesList3 extends React.Component {
   static propTypes = {
     controller: ControllerType.isRequired,
   };
@@ -40,48 +37,7 @@ class SchedulesList extends React.Component {
         width: null,
       }
     ),
-    /*Columns.custom(
-      (text, schedule) => (
-        <Button.Group>
-          <Link.Button href={`schedules/${schedule.id}`}>Edit</Link.Button>
-        </Button.Group>
-      ),
-      {
-        width: "1%",
-        className: "text-nowrap",
-      }
-    ),
-    Columns.custom(
-      (text, schedule) => {
-        return (
-          <DeleteScheduleButton
-            className="w-100"
-            disabled=false
-            schedule={schedule}
-            title=null
-            onClick={() => this.onScheduleDeleted()}>
-            Delete
-          </DeleteScheduleButton>
-        );
-      },
-      {
-        width: "1%",
-        className: "text-nowrap p-l-0",
-        isAvailable: () => currentUser.isAdmin,
-      }
-    ),*/
   ];
-
-  /*createSchedule = () => {
-    CreateScheduleDialog.showModal().onClose(schedule =>
-      Schedule.create(schedule).then(newSchedule => navigateTo(`schedules/${newSchedule.id}`))
-    );
-  };
-
-  onScheduleDeleted = () => {
-    this.props.controller.updatePagination({ page: 1 });
-    this.props.controller.update();
-  };*/
 
   render() {
     const { controller } = this.props;
@@ -95,21 +51,21 @@ class SchedulesList extends React.Component {
 }
 
 const SchedulesListPage = wrapSettingsTab(
-  "Schedules1.List",
+  "Schedules3.List",
   {
     permission: "admin",
-    title: "Schedules1",
-    path: "schedules1",
+    title: "Schedules3",
+    path: "schedules3",
     order: 9,
   },
-  SchedulesList
+  SchedulesList3
 );
 
 routes.register(
-  "Schedules1.List",
+  "Schedules3.List",
   routeWithUserSession({
-    path: "/schedules1",
-    title: "Schedules1",
-    render: pageProps => <SchedulesListPage {...pageProps} currentPage="schedules1" />,
+    path: "/schedules3",
+    title: "Schedules3",
+    render: pageProps => <SchedulesListPage {...pageProps} currentPage="schedules3" />,
   })
 );
