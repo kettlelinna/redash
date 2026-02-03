c794909f63cd562a25997fc919d82ec52a82a6e2
kettlelinna <13501568940@163.com>
2026-02-04 00:31:53 +0800

    fix bug for scheduling

--- redash/tasks/schedule.py
+++ redash/tasks/schedule.py
@@ -129,13 +129,13 @@ def trigger_mqtt(schedules):
     for s in schedules:
         meta = {"topic": s.args["topic"], "message": s.args["message"]}
         connect_info = {"server": s.args["server"], "port": s.args["port"]}
-        if s.objective == "emqx":
-            client = MQTTClient(connect_info["server"], connect_info["port"])
-            client.connect(s.args["username"], s.args["password"])
-            if client.is_connected():
-                if client.publish(meta["topic"], meta["message"]):
-                    logger.info("Done scheduling: %s" % meta)
-                else:
-                    logger.warning("Failed scheduling: %s" % meta)
+
+        client = MQTTClient(connect_info["server"], connect_info["port"])
+        client.connect(s.args["username"], s.args["password"])
+        if client.is_connected():
+            if client.publish(meta["topic"], meta["message"]):
+                logger.info("Done scheduling: %s" % meta)
             else:
-                logger.warning("Cannot connect to mqtt: %s" % connect_info)
+                logger.warning("Failed scheduling: %s" % meta)
+        else:
+            logger.warning("Cannot connect to mqtt: %s" % connect_info)
