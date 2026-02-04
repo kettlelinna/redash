5e4a54b320d143745fdd813c56887803d5c78080
kettlelinna <13501568940@163.com>
2026-02-04 11:08:55 +0800

    debug

--- redash/utils/mqtt.py
+++ redash/utils/mqtt.py
@@ -4,22 +4,11 @@ import paho.mqtt.client as mqtt
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        logger.info("Connected[mqtt] successfully")
-    else:
-        logger.warning(f"Connection[mqtt] failed with code {rc}")
-
-def on_disconnect(client, userdata, rc):
-    logger.warning(f"Disconnected[mqtt] with code {rc}")
-
 class MQTTClient:
     def __init__(self, server, port):
         self.server = server
         self.port = port
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-        self.client.on_connect = on_connect
-        self.client.on_disconnect = on_disconnect
         self.client.enable_logger()
 
     def connect(self, username, password):
